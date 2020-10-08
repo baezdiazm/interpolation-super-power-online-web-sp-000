efec06b341915ee28202e3a5d670159382c7be53
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-10-08 02:57:36 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(arg)
-  puts "R: #{arg}, O: #{arg}, Y: #{arg}, G: #{arg}, B: #{arg}, I: #{arg}, V: #{arg}"
+ colors = []
+ colors << arg
+ puts colors
 end
